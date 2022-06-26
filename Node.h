d4e2908244057bb29396d26df833685c7d54e362
brendap21 <bpna16100224@gmail.com>
2022-06-25 20:31:57 -0500

    Se anaden modificaciones realizadas en funciones del codigo y se crea la clase NODE para simplificar el codigo.

---
+++ Node.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+class Node
+{
+public:
+	int value;
+	Texture texture;
+	Texture estado;
+	//Sprite sprite_fondo;
+	Sprite* sprite_arturo;
+	//Sprite sprite_estado;
+
+
+	Node(string fondo, string ruta_arturo, string ruta_estado, int v) {
+		value = v;
+
+		if (!texture.loadFromFile(fondo)) {
+			system("echo No se pudo cargar la textura del fondo.&pause");
+		}
+		if (!texture.loadFromFile(ruta_arturo)) {
+			system("echo No se pudo cargar la textura de R2.&pause");
+		}
+		if (!texture.loadFromFile(ruta_estado)) {
+			system("echo No se pudo cargar la textura de emocion.&pause");
+		}
+
+		sprite_arturo = new Sprite(texture);
+		//sprite_arturo->setTexture(&texture);
+
+		//sprite_fondo;
+		sprite_arturo->setPosition(25, -25);
+		//sprite_estado.setPosition(672, 490);
+	}
+
+};
+
--- TAMAGOTCHI.cpp
+++ TAMAGOTCHI.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include "Node.h"
 
 using namespace std;
 using namespace sf;
@@ -15,156 +16,22 @@ void instructions() {
     cout << ("Presione la tecla X para salir del juego.\n") << endl;
 }
 
+    //BASE
+
+
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "STAR WARS TAMAGOTCHI");
+    
+    /*RectangleShape fondo(Vector2f(800, 600));
+    RectangleShape ruta_arturo(Vector2f(800, 600));
+    CircleShape ruta_estado(115, 112);*/
 
-    //BASE
-    RectangleShape fondo(Vector2f(800, 600));
-    RectangleShape r2Base(Vector2f(800, 600));
-    //EMOTIONS
-    CircleShape emocionFeliz(115, 112);
-    CircleShape emocionTriste(115, 112);
-    CircleShape emocionEnojado(115, 112);
-    CircleShape emocionMuerto(115, 112);
-    //TASKS
-    RectangleShape r2Alimento(Vector2f(800, 600));
-    RectangleShape r2Ejercicio(Vector2f(800, 600));
-    RectangleShape r2Limpieza(Vector2f(800, 600));
-    RectangleShape r2Pegar(Vector2f(800, 600));
-    RectangleShape r2Muerte(Vector2f(800, 600));
-
-
-    //FONDO GENERAL
-    Texture texture1;
-    if (!texture1.loadFromFile("img/ILUSTRACIONES_FONDO.png")) {
-        system("echo No se pudo cargar el fondo.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture1.loadFromFile("img/ILUSTRACIONES_FONDO.png");
+    /*Texture texture1;
+    if (texture1.loadFromFile("img/ILUSTRACIONES_FONDO.png")) {
         fondo.setTexture(&texture1);
-    Sprite sprite1(texture1);
-
-    //R2 BASE
-    Texture texture2;
-    if (!texture2.loadFromFile("img/ILUSTRACIONES_TAMAGOCHI.png")) {
-        system("echo No se pudo cargar la textura de r2 base.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture2.loadFromFile("img/ILUSTRACIONES_TAMAGOCHI.png");
-    r2Base.setTexture(&texture2);
-    Sprite sprite2(texture2);
-    sprite2.setPosition(25,-25);
-
-    //FELICIDAD
-    Texture texture3;
-    if (!texture3.loadFromFile("img/ILUSTRACIONES_FELIZ.png")) {
-        system("echo No se pudo cargar la textura de felicidad.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture3.loadFromFile("img/ILUSTRACIONES_FELIZ.png");
-    emocionFeliz.setTexture(&texture3);
-    Sprite sprite3(texture3);
-    sprite3.setPosition(672, 490);
-
-    //TRISTEZA
-    Texture texture4;
-    if (!texture4.loadFromFile("img/ILUSTRACIONES_TRISTE.png")) {
-        system("echo No se pudo cargar la textura de Tristeza.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture4.loadFromFile("img/ILUSTRACIONES_TRISTE.png");
-    emocionTriste.setTexture(&texture4);
-    Sprite sprite4(texture4);
-    sprite4.setPosition(672, 490);
-
-    //ENOJO
-    Texture texture5;
-    if (!texture5.loadFromFile("img/ILUSTRACIONES_ENOJADO.png")) {
-        system("echo No se pudo cargar la textura de Enojo.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture5.loadFromFile("img/ILUSTRACIONES_ENOJADO.png");
-    emocionEnojado.setTexture(&texture5);
-    Sprite sprite5(texture5);
-    sprite5.setPosition(672, 490);
-
-    //MUERTO
-    Texture texture6;
-    if (!texture6.loadFromFile("img/ILUSTRACIONES_MUERTO.png")) {
-        system("echo No se pudo cargar la textura de Muerte.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture6.loadFromFile("img/ILUSTRACIONES_MUERTO.png");
-    emocionMuerto.setTexture(&texture6);
-    Sprite sprite6(texture6);
-    sprite6.setPosition(672, 490);
-
-    //ALIMENTO
-    Texture texture7;
-    if (!texture7.loadFromFile("img/ILUSTRACIONES_ALIMENTO.png")) {
-        system("echo No se pudo cargar la textura de Alimento.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture7.loadFromFile("img/ILUSTRACIONES_ALIMENTO.png");
-    r2Alimento.setTexture(&texture7);
-    Sprite sprite7(texture7);
-    sprite7.setPosition(25, -25);
-
-    //EJERCICIO
-    Texture texture8;
-    if (!texture8.loadFromFile("img/ILUSTRACIONES_EJERCICIO.png")) {
-        system("echo No se pudo cargar la textura de Ejercicio.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture8.loadFromFile("img/ILUSTRACIONES_EJERCICIO.png");
-    r2Ejercicio.setTexture(&texture8);
-    Sprite sprite8(texture8);
-    sprite8.setPosition(25, -25);
-
-    //LIMPIEZA
-    Texture texture9;
-    if (!texture9.loadFromFile("img/ILUSTRACIONES_LIMPIEZA.png")) {
-        system("echo No se pudo cargar la textura de Limpieza.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture9.loadFromFile("img/ILUSTRACIONES_LIMPIEZA.png");
-    r2Limpieza.setTexture(&texture9);
-    Sprite sprite9(texture9);
-    sprite9.setPosition(25, -25);
-
-    //PEGAR
-    Texture texture10;
-    if (!texture10.loadFromFile("img/ILUSTRACIONES_PEGAR.png")) {
-        system("echo No se pudo cargar la textura de Golpe.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture10.loadFromFile("img/ILUSTRACIONES_PEGAR.png");
-    r2Pegar.setTexture(&texture10);
-    Sprite sprite10(texture10);
-    sprite10.setPosition(25, -25);
-
-    //MUERTE
-    Texture texture11;
-    if (!texture11.loadFromFile("img/ILUSTRACIONES_MUERTE.png")) {
-        system("echo No se pudo cargar la textura de Muerte.&pause");
-        return EXIT_FAILURE;
-    }
-    else
-        texture11.loadFromFile("img/ILUSTRACIONES_MUERTE.png");
-    r2Muerte.setTexture(&texture11);
-    Sprite sprite11(texture11);
-    sprite11.setPosition(25, -25);
+        Sprite sprite1(texture1);
+    }*/
 
     //MUSIC
     Music music;
@@ -175,32 +42,64 @@ int main()
     else music.openFromFile("audio/StarWarsAudio.wav");
     music.play();
 
+    Node* estadoFeliz = new Node("img/ILUSTRACIONES_FONDO.png", "img/ILUSTRACIONES_TAMAGOCHI.png", "img/ILUSTRACIONES_FELIZ.png",1);
+    Node* estadoActual = estadoFeliz;
+    Node* estadoEnojado = new Node("img/ILUSTRACIONES_FONDO.png", "img/ILUSTRACIONES_PEGAR.png", "img / ILUSTRACIONES_ENOJADO.png",3);
+    Node* estadoTriste = new Node("img/ILUSTRACIONES_FONDO.png", "img/ILUSTRACIONES_TRISTE-13.png", "img/ILUSTRACIONES_TRISTE-11.png",2);
+    Node* estadoFinal = new Node("img/ILUSTRACIONES_FONDO.png", "img/ILUSTRACIONES_MUERTE.png", "img/ILUSTRACIONES_MUERTO.png",4);
+    
+    Node* estados[4][4] = {
+        {estadoFeliz,estadoTriste,estadoFeliz,estadoFeliz},
+        {estadoFeliz,estadoEnojado,estadoTriste,estadoFeliz},
+        {estadoFeliz,estadoFinal,estadoTriste,estadoEnojado},
+        {estadoFinal,estadoFinal,estadoFinal,estadoFinal}
+    };
+
     Thread thread(&instructions);
     thread.launch();
 
     window.setFramerateLimit(30);
 
+    int opcion;
     while (window.isOpen())
     {
         Event event;
+        opcion = 0;
         while (window.pollEvent(event))
         {     
             if (event.type == Event::KeyPressed) {
+                
+                if (event.key.code == Keyboard::Q) {
+                    opcion = 4;
+                    cout << ("\n\nPresiono la tecla Q\n\n") << endl;
+                }
+                if (event.key.code == Keyboard::W) {
+                    opcion = 3;
+                    cout << ("\n\nPresiono la tecla W\n\n") << endl;
+                }
+                if (event.key.code == Keyboard::R) {
+                    opcion = 2;
+                    cout << ("\n\nPresiono la tecla R\n\n") << endl;
+                }
+                if (event.key.code == Keyboard::E) {
+                    opcion = 1;
+                    cout << ("\n\nPresiono la tecla E\n\n") << endl;
+                }
                 if (event.key.code == Keyboard::X) {
                     window.close();
                     cout << ("\n\nPresiono la tecla X\n\n") << endl;
                 }
-            }
-            if (event.type == Event::Closed)
+                if (event.type == Event::Closed)
                 window.close();
+            }
         }
-
+        estadoActual = estados[estadoActual->value][opcion];
+        
+        //estadoActual->sprite_arturo;
         window.clear();
-        window.draw(sprite1);
-        window.draw(sprite2);
-       // window.draw(sprite9);
-       // window.draw(sprite6);
+        window.draw(*estadoActual->sprite_arturo);
         window.display();
     }
+
     return EXIT_SUCCESS;
 }
